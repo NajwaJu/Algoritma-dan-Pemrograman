56e9823e4f05f0c88441fedc267b6b24cbd768cf
NajwaJu <113366760+NajwaJu@users.noreply.github.com>
2022-09-15 19:11:01 +0700

    Add files via upload

---
+++ Python Bank.py
@@ -0,0 +1,13 @@
+def greetings(bilang):
+    bilang = bilang.strip().lower()
+
+    if "hello" in bilang:
+        print ('$0')
+    elif bilang[0] == 'h' :
+        print ('$20')
+    else :
+        print ('$100')
+
+jawab = input("Type Greetings \n")
+
+greetings(jawab)
