a341988d160d7f5dda5ad2bf6de4c5f671e03dac
NajwaJu <113366760+NajwaJu@users.noreply.github.com>
2022-09-21 08:54:36 +0700

    Update Python Bank.py

--- Python Bank.py
+++ Python Bank.py
@@ -1,11 +1,11 @@
 #Talitha Najwa Afifah (220535603474)
 
-def greetings(bilang):
-    bilang = bilang.strip().lower()
+def greetings(katakan):
+    katakan = katakan.strip().lower()
 
-    if "hello" in bilang:
+    if "hello" in katakan:
         print ('$0')
-    elif bilang[0] == 'h' :
+    elif katakan[0] == 'h' :
         print ('$20')
     else :
         print ('$100')
